e05e24c4232b2ab77ac60be5ff6077769a112bbc
andreykokorev <andrey.kokorev.w.dev@gmail.com>
2019-10-05 05:19:47 +0700

    init: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let prevSample = [],
+        currentSample = str;
+    let bracketPairs = new Map(bracketsConfig);
+    str = str.split('');
+
+    while (true) {
+        let i = 1;
+
+        if (prevSample.length === currentSample.length) {
+            return false;
+        }
+
+        prevSample = currentSample;
+
+        if (str.length === 0) {
+            return true;
+        } else if (str.length === 1) {
+            return false;
+        }
+
+        for (let j = 0; j < str.length; j++) {
+            if (j === str.length - 1) {
+                break;
+            }
+            if (bracketPairs.get(str[j]) === str[i]) {
+                str.splice(j, 2);
+                j -= 2;
+                i--;
+                continue;
+            }
+            i++;
+        }
+        currentSample = str;
+    }
 }
